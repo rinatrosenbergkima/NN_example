16569c93170252b10237be0c5a3dee4135b6ffbd
“PTorr” <torrpolakow@gmail.com>
2017-07-24 15:56:47 +0300

    Rinat changed the code to fit the neural-network.pdf example

--- nn_main_rinat.py
+++ nn_main_rinat.py
@@ -17,10 +17,10 @@ np.random.seed(1)
 
 n_hidden = 2
 # randomly initialize our weights with mean 0
-syn0 = 2 * np.random.random((X[0].shape[0], n_hidden)) - 1
-syn0 = np.array([[0.1, 0.2], [-0.1, -0.2]])
-# syn1 = 2 * np.random.random((n_hidden, 1)) - 1
-syn1 = np.array([[0.5], [-0.6], [0.7]])
+# w_ih = 2 * np.random.random((X[0].shape[0], n_hidden)) - 1
+w_ih = np.array([[0.1, 0.2], [-0.1, -0.2]])
+# w_ho = 2 * np.random.random((n_hidden, 1)) - 1
+w_ho = np.array([[0.5], [-0.6], [0.7]])
 
 print('number of inputs: ', X[0].shape[0])
 print('number of hidden neurons: ', n_hidden)
@@ -31,15 +31,18 @@ for j in xrange(1):
 
     # Feed forward through layers 0, 1, and 2
     l0 = X
-    l1 = nonlin(np.dot(l0, syn0))
-    l2 = nonlin(np.dot(l1, syn1))
+    l1 = nonlin(np.dot(l0, w_ih))
+    print("l1",l1)
+    l1 = np.array([np.append(l1,[-1.0])]) #add the bias to the hidden layer
+    print("l1",l1)
+    l2 = nonlin(np.dot(l1, w_ho))
 
     print('-----------------------------')
     print('weights (input-->hidden): ')
-    print(syn0)
+    print(w_ih)
     print('-----------------------------')
     print('weights (hidden-->output): ')
-    print(syn1)
+    print(w_ho)
     print('-----------------------------')
     print('input: ')
     print(l0)
@@ -69,7 +72,7 @@ for j in xrange(1):
     l2_delta = l2_error * nonlin(l2, deriv=True)
 
     # how much did each l1 value contribute to the l2 error (according to the weights)?
-    l1_error = l2_delta.dot(syn1.T)
+    l1_error = l2_delta.dot(w_ho.T)
     print('-----------------------------')
     print('hidden layer error: ')
     print(l1_error)
@@ -79,7 +82,7 @@ for j in xrange(1):
     l1_delta = l1_error * nonlin(l1, deriv=True)
 
     dw_hidden_output = l1.T.dot(l2_delta)
-    dw_input_hidden = l0.T.dot(l1_delta)
+    dw_input_hidden = l0.T.dot(np.array([l1_delta[0][:2]]))
 
     print('-----------------------------')
     print('change in weights hidden-->output: ')
@@ -88,6 +91,6 @@ for j in xrange(1):
     print('change in weights input-->hidden: ')
     print(dw_input_hidden)
 
-    syn1 += l1.T.dot(l2_delta)
-    syn0 += l0.T.dot(l1_delta)
+    w_ho += dw_hidden_output   #l1.T.dot(l2_delta)
+    w_ih += dw_input_hidden    # l0.T.dot(l1_delta)
 
